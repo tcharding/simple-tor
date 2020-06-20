25ca298b4bf1a62ac53f3ddde36a6dae3fa3af86
tcharding <tobin@coblox.tech>
2020-06-20 10:08:56 +1000

    Initialise Tor instance

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# simple-tor
+# Simple Tor
 
-Simple Rust application to demonstrate usage of libtor.
+Simple Rust application to demonstrate usage of libtor to embed a Tor
+instance in a Rust binary.
+
+Currently only initialises Tor, nothing else.
+
+### TODO
+
+1. Act as a client: connect to a well known hidden service i.e., do a
+   HTTP request to an onion addressed web page.
+
+2. Act as a server:
+   - Expose a hidden service
+   - Start a simple webserver
+   - Test using Tor browser
+
 
 ref:
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use libtor::{HiddenServiceVersion, Tor, TorAddress, TorFlag};
+
+fn main() -> Result<(), libtor::Error> {
+    Tor::new()
+        .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
+        .flag(TorFlag::SocksPort(19050))
+        .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
+        .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
+        .flag(TorFlag::HiddenServicePort(
+            TorAddress::Port(8000),
+            None.into(),
+        ))
+        .start()?;
+
+    Ok(())
 }
